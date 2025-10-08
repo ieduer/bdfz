a10cf5961bf12c987b33cc1ab80ff74b56fe4634
gpter <admin@i.pkuschool.edu.cn>
2025-10-08 06:01:35 -0700

    dns

--- sb.sh
+++ sb.sh
@@ -474,7 +474,11 @@ result_vl_vm_hy_tu(){
     public_key=$(cat /etc/s-box/public.key); short_id=$(jq -r '.inbounds[0].tls.reality.short_id[0]' /etc/s-box/sb.json); 
     ws_path=$(jq -r '.inbounds[1].transport.path' /etc/s-box/sb.json); vm_port=$(jq -r '.inbounds[1].listen_port' /etc/s-box/sb.json); 
     tls=$(jq -r '.inbounds[1].tls.enabled' /etc/s-box/sb.json); vm_name=$(jq -r '.inbounds[1].tls.server_name' /etc/s-box/sb.json);
-    vmadd_local=$server_ipcl
+    if [[ "$tls" = "false" ]]; then
+      vmadd_local=$server_ipcl
+    else
+      vmadd_local=$vm_name
+    fi
     hy2_port=$(jq -r '.inbounds[2].listen_port' /etc/s-box/sb.json); local ym; ym=$(cat /root/ieduerca/ca.log 2>/dev/null || true);
     hy2_sniname=$(jq -r '.inbounds[2].tls.key_path' /etc/s-box/sb.json);
     if [[ "$hy2_sniname" = '/etc/s-box/private.key' ]]; then
@@ -482,7 +486,11 @@ result_vl_vm_hy_tu(){
     else
       hy2_name=$ym; hy2_ins=false
     fi
-    cl_hy2_ip=$server_ipcl
+    if [[ "$hy2_ins" = true ]]; then
+      cl_hy2_svr=$server_ipcl
+    else
+      cl_hy2_svr=$hy2_name
+    fi
     tu5_port=$(jq -r '.inbounds[3].listen_port' /etc/s-box/sb.json);
     tu5_sniname=$(jq -r '.inbounds[3].tls.key_path' /etc/s-box/sb.json);
     if [[ "$tu5_sniname" = '/etc/s-box/private.key' ]]; then
@@ -490,7 +498,11 @@ result_vl_vm_hy_tu(){
     else
       tu5_name=$ym; tu5_ins=false
     fi
-    cl_tu5_ip=$server_ipcl
+    if [[ "$tu5_ins" = true ]]; then
+      cl_tu5_svr=$server_ipcl
+    else
+      cl_tu5_svr=$tu5_name
+    fi
 }
 
 resvless(){ echo; white "~~~~~~~~~~~~~~~~~"; vl_link="vless://$uuid@$server_ipcl:$vl_port?encryption=none&flow=xtls-rprx-vision&security=reality&sni=$vl_name&fp=chrome&pbk=$public_key&sid=$short_id&type=tcp&headerType=none#vl-reality-$hostname"; echo "$vl_link" > /etc/s-box/vl_reality.txt; red "🚀 VLESS-Reality"; echo "链接:"; echo -e "${yellow}$vl_link${plain}"; echo "二维码:"; qrencode -o - -t ANSIUTF8 "$vl_link"; }
@@ -547,14 +559,14 @@ gen_clash_sub(){
       "tls": { "enabled": true, "server_name": "${vl_name}", "utls": { "enabled": true, "fingerprint": "chrome" },
                "reality": { "enabled": true, "public_key": "${public_key}", "short_id": "${short_id}" } } },
     { "type": "vmess", "tag": "${tag_vmess}",
-      "server": "${server_ipcl}", "server_port": ${vm_port}, "uuid": "${uuid}", "security": "auto", "packet_encoding": "packetaddr",
+      "server": "${vmadd_local}", "server_port": ${vm_port}, "uuid": "${uuid}", "security": "auto",
       "transport": { "type": "ws", "path": "${ws_path}", "headers": { "Host": ["${vm_name}"] } },
       "tls": { "enabled": ${tls}, "server_name": "${vm_name}", "insecure": false, "utls": { "enabled": true, "fingerprint": "chrome" }, "alpn": ["http/1.1"] } },
     { "type": "hysteria2", "tag": "${tag_hy2}",
-      "server": "${cl_hy2_ip}", "server_port": ${hy2_port}, "password": "${uuid}",
+      "server": "${cl_hy2_svr}", "server_port": ${hy2_port}, "password": "${uuid}",
       "tls": { "enabled": true, "server_name": "${hy2_name}", "insecure": ${hy2_ins}, "alpn": ["h3"] } },
     { "type": "tuic", "tag": "${tag_tuic}",
-      "server": "${cl_tu5_ip}", "server_port": ${tu5_port}, "uuid": "${uuid}", "password": "${uuid}",
+      "server": "${cl_tu5_svr}", "server_port": ${tu5_port}, "uuid": "${uuid}", "password": "${uuid}",
       "congestion_control": "bbr", "udp_relay_mode": "native",
       "tls": { "enabled": true, "server_name": "${tu5_name}", "insecure": ${tu5_ins}, "alpn": ["h3"] } },
     { "tag": "direct", "type": "direct" },
